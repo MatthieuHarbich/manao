26633631fd8032eeb182b7b2ffed2032f6e1a3fd
Matthieu Harbich <matthieu.harbich@gmail.com>
2016-11-02 13:19:34 +0100

    first

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,11 +14,11 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      // .when('/', {
-      //   templateUrl: 'views/main.html',
-      //   controller: 'MainCtrl',
-      //   controllerAs: 'main'
-      // })
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
       // .when('/about', {
       //   templateUrl: 'views/about.html',
       //   controller: 'AboutCtrl',
